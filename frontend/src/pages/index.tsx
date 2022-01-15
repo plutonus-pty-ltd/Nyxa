7eb8ac6ff329d55db40414b9eee061dcc547c0c6
PlutonusDev <joshua@plutonus.dev>
2022-01-15 00:08:38 +0000

    Add link to the blockchain endpoint

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
 	return (
 		<Layout title="Welcome!">
 			<p>Coming Soon!</p>
+			<a className="underline font-semibold text-indigo-700 dark:text-indigo-300" href="https://chain.nyxa.io/">Blockchain base endpoint</a>
 		</Layout>
 	);
 }
