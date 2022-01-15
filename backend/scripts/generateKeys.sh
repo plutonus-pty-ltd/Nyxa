b9af21c04bb82022b7ef07fd31db94360a4fe193
PlutonusDev <joshua@plutonus.dev>
2022-01-15 00:01:19 +0000

    chmod the private key after generating for build scripts

--- backend/scripts/generateKeys.sh
+++ backend/scripts/generateKeys.sh
@@ -2,3 +2,4 @@ echo Enter the same passphrase during all prompts. Passphrase is optional and ca
 sudo apt install openssh -y >/dev/null
 openssl genrsa -des3 -out ./src/keys/privateKey.pem 4096
 openssl rsa -in ./src/keys/privateKey.pem -outform PEM -pubout -out ./src/keys/publicKey.pem
+chmod -R o+rwx ./src/keys/
